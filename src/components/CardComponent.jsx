405257c447733c47654553929179d276fe2ec5a3
Mark <mrkcarlos576@gmail.com>
2024-05-07 21:54:12 +0300

    assignment complete

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -6,13 +6,13 @@ function CardComponent({bot,onHandleClick,onHandleDelete}) {
     
   return (
   
-    <div className=' grid grid-cols-4 gap-4 bg-lime-600  mb-7 columns-4'>
+    <div className=' grid grid-cols-4 gap-5 bg-lime-600  mb-6 columns-4'>
         <div onClick={ () => onHandleClick(bot)} >
        <div className='bg-stone-200 '>
      
           <img src={bot.avatar_url} alt='bot' />
           <div className="px-6 py-4 ">
-              <div className="font-bold text-xl mb-2">{bot.name}</div>
+              <div className="font-bold text-xl mb-3">{bot.name}</div>
               <p className="text-gray-700 text-base truncate">
                 <b>Catchphrase: </b>{bot.catchphrase}
               </p>
